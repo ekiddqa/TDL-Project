26c2648afff2b037269bb22f6aba0a4daedac159
kidd <ekidd@qa.com>
2021-02-12 14:03:58 +0000

    Entities completed with annotation and relationships

--- src/main/java/com/qa/tdl/persistence/domain/ToDo.java
+++ src/main/java/com/qa/tdl/persistence/domain/ToDo.java
@@ -1,11 +1,12 @@
 package com.qa.tdl.persistence.domain;
 
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,9 +21,12 @@ public class ToDo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(unique = true, nullable = false)
+    @NotNull
     private String task;
 
-    @Column(nullable = false)
+    @NotNull
     private boolean complete;
+    
+    @ManyToOne
+    private ToDoList group = null;
 }
