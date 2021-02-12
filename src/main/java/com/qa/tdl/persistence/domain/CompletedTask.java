feceabe46958cb068f7ce1c61cf2520b1fb36c5c
kidd <ekidd@qa.com>
2021-02-12 13:21:25 +0000

    toDo done. CompletedTask entity started

---
+++ src/main/java/com/qa/tdl/persistence/domain/CompletedTask.java
@@ -0,0 +1,24 @@
+package com.qa.tdl.persistence.domain;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+public class CompletedTask {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String task;
+
+}
