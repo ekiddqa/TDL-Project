f4b53b4304f916cc4cff115c976c4dc28c40f09e
kidd <ekidd@qa.com>
2021-02-12 12:08:49 +0000

    init commit

---
+++ src/main/java/com/qa/tdl/TDLApplication.java
@@ -0,0 +1,13 @@
+package com.qa.tdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TDLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TDLApplication.class, args);
+	}
+
+}
